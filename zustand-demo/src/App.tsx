41b92b379a5c1122972fe35d694bfae2a27e6be1
chuenwei0129 <chuenwei0129@gmail.com>
2024-09-11 18:59:39 +0800

    ✨ feat: 学习 zustand 的用法

--- zustand-demo/src/App.tsx
+++ zustand-demo/src/App.tsx
@@ -1,4 +1,9 @@
 import BearBox from './pages/BearBox'
+import CatBox1 from './pages/CatBox1'
+import CatBox2 from './pages/CatBox2'
+import CatBox3 from './pages/CatBox3'
+import CatBox4 from './pages/CatBox4'
+import FoodBox from './pages/FoodBox'
 
 function App() {
   return (
@@ -6,6 +11,13 @@ function App() {
       <h1>Zustand Demo</h1>
       <div>
         <BearBox />
+        <FoodBox />
+      </div>
+      <div>
+        <CatBox1 />
+        <CatBox2 />
+        <CatBox3 />
+        <CatBox4 />
       </div>
     </div>
   )
